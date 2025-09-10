c317e2981651d9c5b075e7d95a0b26e371d6e509
violeta456 <159485730+violeta456@users.noreply.github.com>
2025-09-10 09:56:03 -0500

    Update pregunta_01.py

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,18 +1,11 @@
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
-
-# pylint: disable=import-outside-toplevel
-
-
 def pregunta_01():
    
     p="Hola mundo cruel!"
 
-    
-
     return p
-
 
 if __name__ == "__main__":
     print(pregunta_01())
